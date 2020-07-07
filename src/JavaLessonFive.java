6ab3b1e9a198169adab32506b6fcb04602d3b914
councit <councit@gmail.com>
2020-07-07 15:00:48 -0700

    exeption handleing

--- src/JavaLessonFive.java
+++ src/JavaLessonFive.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
 public class JavaLessonFive {
+
+    static double myPI =  3.14159; //Class Var
+
     public static void main(String[] args){
 
-
+        System.out.println(addThem(2,4));
 
     }
+
+    public static int addThem(int a, int b) {
+
+        return a + b;
+
+    }
+
 }
